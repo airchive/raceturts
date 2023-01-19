11bc81e19e09e703f15d935eebbdee8209e3992e
Airscript <francesco@airscript.it>
2023-01-19 01:31:29 +0100

    refactor: clean up main.py

--- main.py
+++ main.py
@@ -1,46 +1,45 @@
-#!/bin/python3
+from turtle import *
+from random import randint
 
-from turtle import *;
-from random import randint;
-
-speed(10);
-penup();
-goto(-140, 140);
+speed(10)
+penup()
+goto(-140, 140)
 
 for step in range(15):
-  write(step, align='center');
-  right(90);
-  forward(10);
+  write(step, align='center')
+  right(90)
+  forward(10)
+
   for up in range(8):
-    pendown();
-    forward(10);
-    penup();
-    forward(10);
-  backward(170);
-  left(90);
-  forward(20);
-  
-panny = Turtle();
-panny.color('red');
-panny.shape('turtle');
+    pendown()
+    forward(10)
+    penup()
+    forward(10)
 
-airsit = Turtle();
-airsit.color('green');
-airsit.shape('turtle');
+  backward(170)
+  left(90)
+  forward(20)
 
-panny.penup();
-panny.goto(-160, 100);
+panny = Turtle()
+panny.color('red')
+panny.shape('turtle')
 
-for turn in range(10):
-  panny.right(36);
+airscript = Turtle()
+airscript.color('green')
+airscript.shape('turtle')
 
-airsit.penup();
-airsit.goto(-160, 80);
+panny.penup()
+panny.goto(-160, 100)
 
 for turn in range(10):
-  airsit.left(36);
+  panny.right(36)
+
+airscript.penup()
+airscript.goto(-160, 80)
+
+for turn in range(10):
+  airscript.left(36)
 
 for turn in range(100):
-  panny.forward(randint(1,5));
-  airsit.forward(randint(1,5));
-  
+  panny.forward(randint(1,5))
+  airscript.forward(randint(1,5))
